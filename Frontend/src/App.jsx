a77e8547f318760921f6a996a1c2c6bcae964252
sohail28s <sohailzafar4u@gmail.com>
2026-06-01 10:29:33 +0500

    feat: Implement new feature for enhanced user experience and performance improvements

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
  // Your new layout file
+ 
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer'; 
 import MainLayout from './Layout/Layout'
